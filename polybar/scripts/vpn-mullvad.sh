2d6b06723f88476dc7d7d0be9e639004bfd573ac
peterc-s <petercs@skiff.com>
2024-07-31 17:06:49 +0100

    mullvad polybar

---
+++ polybar/scripts/vpn-mullvad.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+MULLVAD_VPN_STATUS=$(mullvad status | tr '[:upper:]' '[:lower:]' | sed 's/://g')
+MULLVAD_VPN_SERVER=$(echo "$MULLVAD_VPN_STATUS" | cut -d ' ' -f3)
+if [[ $MULLVAD_VPN_STATUS == *"connecting"* ]]; then
+    echo "󱗒 Connecting"
+elif [[ $MULLVAD_VPN_STATUS == *"disconnected"* ]]; then
+    echo "󰍀 Disconnected"
+elif [[ $MULLVAD_VPN_STATUS == *"connected"* ]]; then
+    echo "󱚨 $MULLVAD_VPN_SERVER"
+elif [[ $MULLVAD_VPN_STATUS == *"blocked"* ]]; then
+    echo "󱙲 Blocked"
+else
+    mullvad status
+fi
