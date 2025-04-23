0f725637ce571fa511600c8870b83206de220897
peterc-s <petercs@purelymail.com>
2025-04-23 13:08:45 +0100

    Update 2025-04-23.

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = "Telescope - find files." })
+vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = "Telescope - live grep." })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = "Telescope - buffers." })
+vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = "Telescope - help tags." })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope - git." })
--- nvim/lua/pete/packer.lua
+++ nvim/lua/pete/packer.lua
@@ -1,76 +1,76 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use({
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  })
+    use({
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    })
 
-  use({ 'navarasu/onedark.nvim' })
+    use({ 'navarasu/onedark.nvim' })
 
-  use({ 'nvim-treesitter/nvim-treesitter', { run =  ':TSUpdate' }})
-  use({ 'nvim-treesitter/playground' })
+    use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
+    use({ 'nvim-treesitter/playground' })
 
-  use({ 'ThePrimeagen/harpoon' })
+    use({ 'ThePrimeagen/harpoon' })
 
-  use({ 'mbbill/undotree' })
+    use({ 'mbbill/undotree' })
 
-  use({ 'tpope/vim-fugitive' })
+    use({ 'tpope/vim-fugitive' })
 
-  use({
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    use({
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-          {'saadparwaiz1/cmp_luasnip'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  })
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    })
 
-  use({ 'nvim-lualine/lualine.nvim' })
+    use({ 'nvim-lualine/lualine.nvim' })
 
-  use({ 'folke/which-key.nvim' })
+    use({ 'folke/which-key.nvim' })
 
-  use({ 'numToStr/Comment.nvim' })
+    use({ 'numToStr/Comment.nvim' })
 
-  use({ 'simrat39/rust-tools.nvim' })
+    use({ 'simrat39/rust-tools.nvim' })
 
-  use({ 'm4xshen/autoclose.nvim' })
+    use({ 'm4xshen/autoclose.nvim' })
 
-  use({ 'kylechui/nvim-surround', tag = '*' })
+    use({ 'kylechui/nvim-surround', tag = '*' })
 
-  use({ 'tikhomirov/vim-glsl' })
+    use({ 'tikhomirov/vim-glsl' })
 
-  use({ 'tadmccorkle/markdown.nvim' })
+    use({ 'tadmccorkle/markdown.nvim' })
 
-  use({ 'lervag/vimtex' })
+    use({ 'lervag/vimtex' })
 
-  use({ 'norcalli/nvim-colorizer.lua' })
+    use({ 'norcalli/nvim-colorizer.lua' })
 
-  use({
-      "giusgad/pets.nvim",
-      requires = {
-          "giusgad/hologram.nvim",
-          "MunifTanjim/nui.nvim",
-      }
-  })
+    use({
+        "giusgad/pets.nvim",
+        requires = {
+            "giusgad/hologram.nvim",
+            "MunifTanjim/nui.nvim",
+        }
+    })
 
-  use({
-      "L3MON4D3/LuaSnip",
-      tag = "v2.*",
-      run = "make install_jsregexp"
-  })
+    use({
+        "L3MON4D3/LuaSnip",
+        tag = "v2.*",
+        run = "make install_jsregexp"
+    })
 end)
