cbd2f1c9a98bf5cd5ab6e0ac0e2a60496ebf5167
peterc-s <petercs@purelymail.com>
2026-01-11 23:40:00 +0000

    "60 2026-01-11 23:39:57 25.11.3695.d03088749a11 6.12.64"

--- nvim/after/plugin/typst-preview.lua
+++ nvim/after/plugin/typst-preview.lua
@@ -1,6 +1,6 @@
 require("typst-preview").setup({
   dependencies_bin = {
-    ["tinymist"] = "/run/current-system/sw/bin/tinymist"  
+    ["tinymist"] = "/run/current-system/sw/bin/tinymist",
     ["websocat"] = "/run/current-system/sw/bin/websocat"  
   },
 })
