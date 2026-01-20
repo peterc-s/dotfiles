8a97a534991a6792178b4c6400fde4029f4d4d65
peterc-s <petercs@purelymail.com>
2026-01-20 18:27:43 +0000

    Fix syntax error

--- nixos/modules/features/zram.nix
+++ nixos/modules/features/zram.nix
@@ -12,4 +12,3 @@
     memoryPercent = 50;
   };
 }
-
--- nixos/modules/sw/librewolf.nix
+++ nixos/modules/sw/librewolf.nix
@@ -11,26 +11,26 @@
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
-      ExtensionSettings = with builtins;
-        let extension = shortId: uuid: {
+      ExtensionSettings = with builtins; let
+        extension = shortId: uuid: {
           name = uuid;
           value = {
             install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
             installation_mode = "normal_installed";
           };
         };
-        in listToAttrs [
+      in
+        listToAttrs [
           (extension "ublock-origin" "uBlock0@raymondhill.net")
           (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
           (extension "libredirect" "7esoorv3@alefvanoon.anonaddy.me")
           (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
         ];
-        # To add additional extensions, find it on addons.mozilla.org, find
-        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
-        # Then, download the XPI by filling it in to the install_url template, unzip it,
-        # run `jq .browser_specific_settings.gecko.id manifest.json` or
-        # `jq .applications.gecko.id manifest.json` to get the UUID
-    };
+      # To add additional extensions, find it on addons.mozilla.org, find
+      # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+      # Then, download the XPI by filling it in to the install_url template, unzip it,
+      # run `jq .browser_specific_settings.gecko.id manifest.json` or
+      # `jq .applications.gecko.id manifest.json` to get the UUID
       FirefoxHome = {
         "Search" = false;
       };
