781c73454548a436ba7d9bafb3e2b578130d8071
peterc-s <petercs@purelymail.com>
2026-01-20 18:25:26 +0000

    Better librewolf configuration from discourse

--- nixos/modules/sw/librewolf.nix
+++ nixos/modules/sw/librewolf.nix
@@ -11,18 +11,26 @@
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
-      ExtensionSettings = {
-        # uBlock Origin
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
+      ExtensionSettings = with builtins;
+        let extension = shortId: uuid: {
+          name = uuid;
+          value = {
+            install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
         };
-        # BitWarden - FIXME
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
+        in listToAttrs [
+          (extension "ublock-origin" "uBlock0@raymondhill.net")
+          (extension "bitwarden-password-manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "libredirect" "7esoorv3@alefvanoon.anonaddy.me")
+          (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
+        ];
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then, download the XPI by filling it in to the install_url template, unzip it,
+        # run `jq .browser_specific_settings.gecko.id manifest.json` or
+        # `jq .applications.gecko.id manifest.json` to get the UUID
+    };
       FirefoxHome = {
         "Search" = false;
       };
