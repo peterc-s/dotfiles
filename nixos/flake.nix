6d23e846437dd1389e0a674bf9856e324700f622
peterc-s <petercs@purelymail.com>
2026-01-24 12:48:24 +0000

    Add noctalia-shell ready for PC

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -13,6 +13,11 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
