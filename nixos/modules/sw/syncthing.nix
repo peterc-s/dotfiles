06211409289b8e6663006b5a7bf6adb0edf856fc
peterc-s <petercs@purelymail.com>
2026-01-25 17:19:29 +0000

    Fix syncthing

--- nixos/modules/sw/syncthing.nix
+++ nixos/modules/sw/syncthing.nix
@@ -4,9 +4,14 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  username = "pete";
+in {
   services.syncthing = {
     enable = true;
+    user = username;
+    dataDir = "/home/${username}";
+    configDir = "/home/${username}/.config/syncthing";
     openDefaultPorts = true;
   };
 }
